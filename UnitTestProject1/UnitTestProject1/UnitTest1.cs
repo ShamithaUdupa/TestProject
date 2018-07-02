7e14dfba27a2d689c07310ee4e87fbdc46a76b10
Shamitha <t-shudup@microsoft.com>
2018-07-02 09:44:11 +0530

    Project

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, 2);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(6, 6);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 10);
+        }
+
+    }
+}
